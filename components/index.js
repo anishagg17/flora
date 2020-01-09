6c7e2a7dd98b1b3f62290cb2e4bc16575cda53cd
Anish Aggarwal <anish17122000@gmail.com>
2020-01-09 22:41:41 +0100

    Single product view

--- components/index.js
+++ components/index.js
@@ -1,11 +1,11 @@
 import Block from "./Block";
-// import Badge from "./Badge";
+import Badge from "./Badge";
 import Button from "./Button";
-// import Card from "./Card";
+import Card from "./Card";
 import Input from "./Input";
 import Text from "./Text";
 // import Progress from "./Progress";
-// import Divider from "./Divider";
+import Divider from "./Divider";
 // import Switch from "./Switch";
 
-export { Block, Button, Text, Input };
+export { Block, Button, Text, Input, Divider, Card, Badge };
